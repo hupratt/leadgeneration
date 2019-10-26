b8d8c2347f79f52e0032d4eb08ebe58b8996288a
hupratt <hp@hp.com>
2019-10-26 19:44:03 +0200

    create serializer for the leads

---
+++ leadmanager/leads/api.py
@@ -0,0 +1,18 @@
+from .models import Lead
+from rest_framework import viewsets, permissions
+from .serializers import LeadSerializer
+
+# Lead Viewset
+
+
+class LeadViewSet(viewsets.ModelViewSet):
+    # permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.AllowAny]
+    serializer_class = LeadSerializer
+    queryset = Lead.objects.all()
+
+    # def get_queryset(self):
+    #     return self.request.user.leads.all()
+
+    # def perform_create(self, serializer):
+    #     serializer.save(owner=self.request.user)
